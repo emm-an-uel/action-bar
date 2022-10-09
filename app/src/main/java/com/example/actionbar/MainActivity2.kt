1ad2b669958458b5ef42a34c1d9362415fc30342
emmanuel <sunartio.emmanuel@gmail.com>
2022-10-09 19:05:21 +1030

    completed: pass subject-color combo to MainActivity2

--- app/src/main/java/com/example/actionbar/MainActivity2.kt
+++ app/src/main/java/com/example/actionbar/MainActivity2.kt
@@ -7,10 +7,15 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity2 : AppCompatActivity() {
 
     lateinit var btnActivity1: Button
+    lateinit var listSubjectColor: ArrayList<SubjectColor>
+    lateinit var tvEnglish: TextView
+    lateinit var tvMaths: TextView
+    lateinit var tvScience: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +29,9 @@ class MainActivity2 : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        // get data from intent
+        getFromIntent()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -39,6 +47,27 @@ class MainActivity2 : AppCompatActivity() {
 
                 return true
             } else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun getFromIntent() {
+        val bundle = intent.extras
+        if (bundle != null) {
+            listSubjectColor = bundle.getParcelableArrayList("listSubjectColor")!!
+
+            tvEnglish = findViewById(R.id.tvEnglish)
+            tvMaths = findViewById(R.id.tvMaths)
+            tvScience = findViewById(R.id.tvScience)
+
+            for (item in listSubjectColor) {
+                if (item.subject == "english") {
+                    tvEnglish.text = "English: ${item.color}"
+                } else if (item.subject == "maths") {
+                    tvMaths.text = "Maths: ${item.color}"
+                } else if (item.subject == "science") {
+                    tvScience.text = "Science: ${item.color}"
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/actionbar/SettingsActivity.kt
+++ app/src/main/java/com/example/actionbar/SettingsActivity.kt
@@ -1,11 +1,14 @@
 package com.example.actionbar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TableLayout
 import android.widget.TableRow
+import androidx.core.os.bundleOf
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class SettingsActivity : AppCompatActivity() {
@@ -13,6 +16,7 @@ class SettingsActivity : AppCompatActivity() {
     lateinit var tableLayout: TableLayout
     lateinit var fabNewColor: FloatingActionButton
     lateinit var btnConfirm: Button
+    lateinit var idMap: MutableMap<Int, Int>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +25,7 @@ class SettingsActivity : AppCompatActivity() {
         tableLayout = findViewById(R.id.tableLayout)
         fabNewColor = findViewById(R.id.fabNewColor)
         btnConfirm = findViewById(R.id.btnConfirm)
+        idMap = mutableMapOf()
 
         fabNewColor.setOnClickListener {
             newRow()
@@ -35,6 +40,10 @@ class SettingsActivity : AppCompatActivity() {
         val tableRow = TableRow(this)
         val etSubject = EditText(this)
         val etColor = EditText(this)
+
+        etSubject.id = View.generateViewId()
+        etColor.id = View.generateViewId()
+        idMap.put(etSubject.id, etColor.id) // put etSubject.id with corresponding etColor.id in a map
 
         etSubject.layoutParams = TableRow.LayoutParams(
             TableRow.LayoutParams.WRAP_CONTENT,
@@ -55,8 +64,23 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     private fun confirmColors() {
-        val mapColors = mutableMapOf<String, String>()
 
-        // TODO: implement this
+        val listSubjectColor = arrayListOf<SubjectColor>()
+
+        for ((subjectID, colorID) in idMap) {
+            val etSubject = findViewById<EditText>(subjectID)
+            val etColor = findViewById<EditText>(colorID)
+
+            val subject = etSubject.text.toString()
+            val color = etColor.text.toString()
+
+            val subjectColor = SubjectColor(subject, color)
+            listSubjectColor.add(subjectColor)
+        }
+
+        val intent = Intent(this, MainActivity2::class.java)
+        intent.putExtra("listSubjectColor", listSubjectColor)
+        startActivity(intent)
+        finish()
     }
 }
